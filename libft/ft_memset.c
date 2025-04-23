7d4a48b4e9ec14a722d6b4d008f6c294eff9c3a6
Arsela Bita <abita@c3r4p5.42vienna.com>
2025-04-23 17:03:50 +0200

    fixed data types declaration

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -10,7 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_memset(void *str, int c, size_t n)
+#include "libft.h"
+
+void *ft_memset(void *str, int c, size_t len)
 {
+	size_t i;
+	unsigned char *ptr;
 	
+	i = 0;
+	ptr = (unsigned char *)str;
+	while (i < len)
+	{
+		ptr[i] = (unsigned char)c;
+		i++;
+	}
+	return (str);
 }
