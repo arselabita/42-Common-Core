e24d35d750f45a0dc53a946eb948fa8ee8f6b04e
Arsela Bita <abita@c3r4p5.42vienna.com>
2025-04-23 11:51:37 +0200

    created ft_memset.c declared the function

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_memset(void *b, int c, size_t len)
+void *ft_memset(void *str, int c, size_t n)
 {
 	
 }
