d95092ae5d8aa7718bf7e33a74c3311b2d1c9d97
Arsela Bita <arselabita@gmail.com>
2025-04-27 18:15:08 +0200

    removed main

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*str1;
@@ -31,23 +29,5 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	
 	if (i < n)
 		return (str1[i] - str2[i]);
-	return (0);
-}
-
-int	main()
-{
-	char	str1[] = { 'a', 'r', '\0', 's', 'e', 'l', 'a' };
-	char	str2[] = { 'a', 'r', '\200', 's', 'e', 'l', 'a' };
-	int	value;
-
-	value = ft_memcmp(str1, str2, sizeof(str2));
-	printf("The value is: %d\n", value);
-
-	if (value == 0)
-		printf("The strings are equal.\n");
-	else  if (value > 0)
-		printf("str1 is greater than str2\n");
-	else
-		printf("str1 is less than str2\n");
 	return (0);
 }
