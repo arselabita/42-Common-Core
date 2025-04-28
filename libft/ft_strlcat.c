fec16d24bc6a6f06fd8390c0c918c9d15fc2d8f8
Arsela Bita <arselabita@gmail.com>
2025-04-28 12:13:51 +0200

    finallyy

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -25,16 +25,13 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	srclen;
 	size_t	dstlen;
-	size_t	length;
-	size_t	j;
 	size_t	i;
 
 	dstlen = ft_strlen(dst);
 	srclen = ft_strlen(src);
 
-	length = dstlen + srclen;
-	if (dstlen == size)
-		return (length);
+	if (size <= dstlen)
+		return (size + srclen);
 	i = 0;
 	while (src[i] && (dstlen + i < size - 1))
 	{
@@ -42,19 +39,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 		i++;
 	}
 	dst[dstlen + i] = '\0';
-	return (length);
-}
-
-int	main()
-{
-	char	src[] = "arsecdekkkkkkkkkkkf";
-	char	dest[12] = "bita";
-
-	size_t a = ft_strlcat(dest, src, sizeof(dest));
-
-	printf("%s\n", dest);
-	printf("%zu\n", a);
-
-	return (0);
+	return (dstlen + srclen);
 }
 
