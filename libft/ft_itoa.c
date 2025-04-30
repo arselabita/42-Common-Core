f09aa2475782b854a1692578209b78abf4a76ab7
Arsela Bita <arselabita@gmail.com>
2025-04-30 13:46:44 +0200

    unfinished work

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -19,61 +19,36 @@ int	ft_numlen(int n)
 	int	i;
 
 	i = 0;
-	if (n > 0)
+	while (n != 0)
 	{
-		i += 1;
 		return (ft_numlen(n/10));
+		i++;
 	}
-	else
-		return (i);
-}
-
-void	ft_putnbr(int nb)
-{
-	char	print;
-	long int	n;
-	
-	n = nb;
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		n = -n;
-	}
-	if (n >= 10)
-		ft_putnbr(n / 10);
-	print = (n % 10) + '0';
-	write(1, &print, 1);
+	return (i);
 }
 
 char	*ft_itoa(int n)
 {
-	char		*converted;
-	char		print;
 	int		i;
 	long int	nb;
-	
+	char		*converted;
 
 	converted = (char *)malloc((ft_numlen(n) + 1) * sizeof(char));
 	if (converted == NULL)
 		return (NULL);
 	if (n == 0)
-		converted[i] = '\0';
+		nb = '\0';
 	nb = n;
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		n = -n;
-	}
-	if (n >= 10)
-		ft_putnbr(n / 10);
-	print = (n % 10) + '0';
-	write(1, &print, 1);
+	if (nb < 0)
+		nb = -nb;
 	i = 0;
-	while(converted[i] >= 48 && converted[i] <= 57)
+	while(nb >= 0 && nb <= 9)
 	{
-		ft_putnbr(converted);
+		if (nb >= 10)
+			nb /= 10;
+		converted[i] = (nb % 10) + '0';
+		i++;
 	}
-
 	converted[i] = '\0';
 	return(converted);
 }
@@ -83,7 +58,8 @@ int	main()
 	int	num;
 	char	*value;
 
-	value = ft_itoa(9);
+	num = 42;
+	value = ft_itoa(num);
 	if (value != NULL)
 	{
 		printf("The number: %d\n", num);
