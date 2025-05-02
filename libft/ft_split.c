cbd29ab12b825f117a9c3fa70cdeb56b2858c479
Arsela Bita <arselabita@gmail.com>
2025-05-02 18:16:23 +0200

    getting started

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -14,10 +14,40 @@
 
 char	**ft_split(char const *s, char c)
 {
-	
+	int	i;
+	char	**split;
+
+	split = (char **)malloc(ft(ft_strlen(s)) * sizeof(char *));
+	if (split == NULL) return (NULL);
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (split);
 }
 
 int	main()
 {
+	char	str[] = "Returns NULL if the allocation fails.";
+	char	ch = 'a';
+	char	**split;
+
+	split = ft_split(str, ch);
 	
-}
+	//maybe free the allocated memory properly (loop)
+	if (split != NULL)
+	{
+		printf("The input: %s\n", str);         
+		printf("The delimiter: %c\n", ch);
+		printf("The output: %s\n", split);
+		free(split);
+	}
+	else
+	{
+		printf("Memory allocation failed.\n");
+		free(split);
+	}
+	return (0);
+}                  
