38c5aa9198977ec24e417bcc3ee0df7aea5a3a20
Arsela Bita <arselabita@gmail.com>
2025-05-02 14:43:15 +0200

    such a beautiful idea

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -10,12 +10,65 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+
+char	funct(unsigned int i, char ch)
+{
+	if ((i % 2 == 0) && (ch >= 'a' && ch <= 'z'))
+		return (ch - 32);
+	else if ((i % 2 != 0) && (ch >= 'A' && ch <= 'Z'))
+		return (ch + 32);
+	else
+		return (ch);
+}
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	int	i;
+	char	*new_str;
+
+	new_str = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if(new_str == NULL) return (NULL);
+
+	i = 0;
+	while (s[i])
+	{
+		new_str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
 }
 
 int	main()
 {
+	char	str[] = "Returns NULL if the allocation fails.";
+	char	*value;
+
+	value = ft_strmapi(str, funct);
+	if (value != NULL)
+	{
+		printf("The input: %s\n", str);
+		printf("The output: %s\n", value);
+		free(value);
+	}
+	else
+	{
+		printf("Memory allocation failed!");
+		free(value);
+	}
 	return (0);
 }
