a7ecb0f68ca2cbad4880e35f6be4899b18c76bbb
Arsela Bita <arselabita@gmail.com>
2025-05-03 21:08:41 +0200

    made some changes, hopefully for good

--- libft/libft.h
+++ libft/libft.h
@@ -26,10 +26,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 int	ft_atoi(const char *nptr);
 
-size_t	ft_strlen(const char *str);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
-
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
@@ -37,6 +33,9 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 char	*ft_itoa(int n);
 void	*ft_memset(void *str, int c, size_t len);
