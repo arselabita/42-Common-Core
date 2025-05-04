1c45159ca2c532fce23e76510432f4f72d5c95cc
Arsela Bita <arselabita@gmail.com>
2025-05-04 16:37:38 +0200

    norm check and test case

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -11,45 +11,43 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t		i;
-	unsigned char	ch;
-	unsigned char	*str;
+	size_t				i;
+	unsigned char		ch;
+	const unsigned char	*str;
 
 	ch = (unsigned char) c;
 	str = (unsigned char *)s;
-
 	i = 0;
 	while (i < n)
 	{
 		if (str[i] == ch)
-			return &str[i];
+			return ((void *)&str[i]);
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
 /*
-#include <stdio.h>
-
-int	main()
+int	main(void)
 {
-	int	index;
-	char	str[10000] = "abita";
+	int		index;
+	char	str[] = "abita";
 	char	*value;
+
 	printf("The input: %s\n", str);
-	value = ft_memchr(str, 't', sizeof(str));
-	
+	value = ft_memchr(str, 't', ft_strlen(str));
+
 	if (value != NULL)
 	{
 		printf("The char found: %s\n", value);
 		index = (int)(value - str);
-		printf("the index: %d\n", index);
+		printf("The index: %d\n", index);
 	}
 	else
-		printf("The char is not found.");
+		printf("The char is not found.\n");
 
 	return (0);
 }*/
-
--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -11,25 +11,33 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+//#include <stdio.h>
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*str1;
 	unsigned char	*str2;
-	size_t		i;
+	size_t			i;
 
 	str1 = (unsigned char *) s1;
 	str2 = (unsigned char *) s2;
-
 	i = 0;
 	while (i < n)
 	{
 		if (str1[i] != str2[i])
-			return (str1[i]  - str2[i]);
+			return (str1[i] - str2[i]);
 		i++;
 	}
-	
-	if (i < n)
-		return (str1[i] - str2[i]);
 	return (0);
 }
+/*
+int main()
+{
+	char s1[] = "abcdef";
+	char s2[] = "abcxef";
+
+	printf("ft_memcmp:%d\n", ft_memcmp(s1, s2, 3));
+	printf("ft_memcmp:%d\n", ft_memcmp(s1, s2, 5));
+	printf("ft_memcmp:%d\n", ft_memcmp(s1, s2, 0));
+	return (0);
+}*/
