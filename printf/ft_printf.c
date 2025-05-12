7bf6ba07d338d27ca37941ba3bc951c89d3981e3
Arsela Bita <arselabita@gmail.com>
2025-05-12 13:21:16 +0200

    handeling format specifiers

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -12,93 +12,93 @@
 
 #include "libft.h"
 #include <stdarg.h>
+#include <stdio.h>
 
-static void	ft_putchar(char c)
+static void ft_putchar(char c)
 {
-	write(1, &c, 1);
+    write(1, &c, 1);
 }
-static void	ft_putnbr(int nb)
+static void	ft_putstr(char *str)
 {
-	char	    print;
-	long int	n;
-	
-	n = nb;
-	if (n < 0)
+	int	i;
+
+	i = 0;
+	while (str[i])
 	{
-		write(1, "-", 1);
-		n = -n;
+		write(1, &str[i], 1);
+		i++;
 	}
-	if (n >= 10)
-		ft_putnbr(n / 10);
-	print = (n % 10) + '0';
-	write(1, &print, 1);
 }
 
-int ft_printf(const char* format, ...)
+static void ft_putnbr(int nb)
+{
+    char print;
+    long int n;
+
+    n = nb;
+    if (n < 0)
+    {
+        write(1, "-", 1);
+        n = -n;
+    }
+    if (n >= 10)
+        ft_putnbr(n / 10);
+    print = (n % 10) + '0';
+    write(1, &print, 1);
+}
+
+int ft_printf(const char *format, ...)
 {
     va_list args;
-    int     next_arg;
-    int     i;
-    int     j;
+    int i;
 
     va_start(args, format);
-    //ketu iteroj ne te gjithe format stringun qe do te shfaqet ne console
+
     i = 0;
-    j = 0;
-    next_arg = 0;
+    // ketu iteroj ne te gjithe format stringun qe do te shfaqet ne console
     while (format[i] && i < ft_strlen(format) - 1)
     {
-        //kusht nese ne hasim nje % 
+        // kusht nese ne hasim nje %
         if (format[i] == '%')
+        {
             i++;
             // konverto ne decimal ose int
-            if (format[i + 1] == 'd' || format[i + 1] == 'i')
-            {
-                next_arg = va_arg(args, int);
-                ft_putnbr(next_arg);
-            }
+            if (format[i] == 'd' || format[i] == 'i')
+                ft_putnbr(va_arg(args, int));
             // konverto ne char
-            else if (format[i + 1] == 'c')
-            {
-                next_arg = va_arg(args, int);            
-                ft_putchar(next_arg);    
-            }
+            else if (format[i] == 'c')
+                ft_putchar(va_arg(args, int));
             // konverto ne string
-            else if (format[i + 1] == 's')
-            {
-                next_arg = va_arg(args, char *);            
-                ft_putchar(next_arg);
-            }
-            else if (format[i + 1] == 'p')
-            {
-                next_arg = va_arg(args, char *);            
-                ft_putchar(next_arg);
-            }
-            else if (format[i + 1] == 'u')
+            else if (format[i] == 's')
+                ft_putstr(va_arg(args, int));
+            // konverto ne pointer
+            else if (format[i] == 'p')
             {
 
             }
-            else if (format[i + 1] == 'x')
+            else if (format[i] == 'u')
             {
 
             }
-            else if (format[i + 1] == 'X')
+            else if (format[i] == 'x')
             {
 
             }
-            else if (format[i + 1] == '%')
+            else if (format[i] == 'X')
             {
-
+                
             }
-            else
-            {
-                ft_putchar(format[i]);
-            }
-            va_end(args);
-            i++;
+            else if (format[i] == '%')
+                ft_putchar('%');
+        }
+        else
+            ft_putchar(format[i]);
+        i++;
     }
-    return (next_arg);
+    va_end(args);
+    return (0);
 }
+
 int main()
 {
     ft_printf("Hi, I'm %s", "Arsela");
