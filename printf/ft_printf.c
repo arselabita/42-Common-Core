61c5832d0dbf28767272a6bad0990e7ac3e6a444
Arsela Bita <arselabita@gmail.com>
2025-05-12 12:01:46 +0200

    handeling format specifiers

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -16,6 +16,7 @@
 int ft_printf(const char* format, ...)
 {
     va_list args;
+    int next_arg;
     char    buffer[20];
     int     i;
     int     j;
@@ -25,21 +26,46 @@ int ft_printf(const char* format, ...)
     //ketu iteroj ne te gjithe format stringun qe do te shfaqet ne console
     i = 0;
     j = 0;
+    next_arg = 0;
     while (format[i] && i < ft_strlen(format) - 1)
     {
         //kusht nese ne hasim nje % 
-        if (ft_strchr(format, '%'))
-            if (format == 'd')
+        if (format[i] == '%')
+            if (format[i] == 'd' || format[i] == 'i')
+                next_arg = ft_itoa(va_arg(args, int));
+            else if (format[i] == 'c')
             {
-                int next_arg = va_arg(args, int);
+
             }
+            else if (format[i] == 's')
+            {
+
+            }
+            else if (format[i] == 'p')
+            {
+
+            }
+            else if (format[i] == 'u')
+            {
+
+            }
+            else if (format[i] == 'x')
+            {
+
+            }
+            else if (format[i] == 'X')
+            {
+
+            }
+            else
+            
             va_end(args);
             i++;
         // duhet te vendos cdo element ne output format.
         buffer[j++] = format[i++];
     }
     buffer[j] = '\0';
-    return (buffer);
+    return (next_arg);
 }
 int main()
 {
